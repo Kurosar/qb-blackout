39899c519ef8919d061b8a0e3f2ac4585e1783f7
Kurøsår Drækåvik <kurosar.draekavik@protonmail.com>
2021-08-03 10:40:57 +0200

    Update config.lua

--- config.lua
+++ config.lua
@@ -2,7 +2,7 @@ Config = {}
 
 -- QB-BLACKOUT CONFIGURATION
 Config.volume  = 0.3 -- Volume for the sounds played clientside
-Config.notificationtext_on = "Power outtage ended" -- Text to show on the notification when the blackout starts
-Config.notificationtext_off = "Power outtage started" -- Text to show on the notification when the blackout ends
+Config.notificationtext_on = "Power outage ended" -- Text to show on the notification when the blackout starts
+Config.notificationtext_off = "Power outage started" -- Text to show on the notification when the blackout ends
 Config.notificationtime = 10000 -- Time in ms to show the notification for clients
 Config.notificationenable = true -- Enable/Disable the notification for clients
